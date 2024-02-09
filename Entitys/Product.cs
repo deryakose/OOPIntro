b94a33e3d8fb39a27681d9f02af5caadb582292d
derko <derko@DKOSE>
2024-02-09 12:54:12 +0300

    Add project files.

---
+++ Entitys/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPIntro.Entitys
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int UnitPrice { get; set; }
+        public double DiscountRate { get; set; }
+        public double DiscountedPrice { get; }
+    }
+}
